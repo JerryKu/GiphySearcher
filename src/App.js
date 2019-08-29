e1eda33c48bf8adcf33f79db66309785938744d3
JerryKu <jerryku1220@gmail.com>
2019-08-29 02:51:46 -0700

    added load more functionality

--- src/App.js
+++ src/App.js
@@ -11,9 +11,12 @@ class App extends React.Component {
     super(props);
 
     this.state = {
-      imageResults: []
+      currentSearch: "",
+      imageResults: [],
+      imageOffset: 15
     }
     this.onSearch = this.onSearch.bind(this);
+    this.onLoadMore = this.onLoadMore.bind(this);
   }
 
   componentDidMount() {
@@ -26,10 +29,26 @@ class App extends React.Component {
   }
 
   onSearch(searchTerm) {
-    axios.get('http://api.giphy.com/v1/gifs/search?api_key=e5IHMLQpsdQwrtrQyJiDibOYStCTgm3T&q=' + searchTerm)
+    axios.get('http://api.giphy.com/v1/gifs/search?api_key=e5IHMLQpsdQwrtrQyJiDibOYStCTgm3T&limit=15&q=' + searchTerm)
     .then((response)=> {
       this.setState({
+          currentSearch: searchTerm,
           imageResults: response.data.data
+      })
+    })
+  }
+
+  onLoadMore(){
+    axios.get('http://api.giphy.com/v1/gifs/search?api_key=e5IHMLQpsdQwrtrQyJiDibOYStCTgm3T&limit=15&offset=' +
+      this.state.imageOffset +
+     '&q=' + this.state.currentSearch)
+    .then((response)=> {
+      let currentResults = this.state.imageResults;
+      let imageOffset = this.state.imageOffset + 15;
+      currentResults = currentResults.concat(response.data.data)
+      this.setState({
+          imageResults: currentResults,
+          imageOffset: imageOffset
       })
     })
   }
@@ -40,6 +59,7 @@ class App extends React.Component {
         <header className="App-header">
         <SearchBar onSearch={this.onSearch}/>
         <SearchResults imageResults={this.state.imageResults}/>
+        <input type="button" value="Load More" onClick={this.onLoadMore}></input>
         </header>
       </div>
     );
